2d8a2589c6d6dbf31c8a838dce986b2164c6d654
luke23571113 <luke23571113@gmail.com>
2022-10-21 16:26:10 -0500

    updated dependencies

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from nvdiffmodeling.src import obj
+from nvdiffmodeling.src import mesh
 
+load_mesh = obj.load_obj("chair.obj")
 
+load_mesh = mesh.unit_size(load_mesh) #I think this normalizes it??
 
 
 #need to normalize first ...  
