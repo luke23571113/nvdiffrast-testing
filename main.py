512a3d17ffe2a7090042125e3996bef1447caf1c
luke23571113 <luke23571113@gmail.com>
2022-10-22 22:56:01 -0500

    waiting for node...

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from nvdiffmodeling.src import obj
 from nvdiffmodeling.src import mesh
 
+import nvdiffrast.torch as dr
+
 load_mesh = obj.load_obj("chair.obj")
 
 load_mesh = mesh.unit_size(load_mesh) #I think this normalizes it??
-
 
 #need to normalize first ...  
